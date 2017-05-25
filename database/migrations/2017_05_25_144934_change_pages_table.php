6f552dc5847f86f06f8ce0d90640b3ccc433b685
Andrei Yechkalov <echkalov@ukr.net>
2017-05-25 23:22:12 +0300

    up

---
+++ database/migrations/2017_05_25_144934_change_pages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->string('images',100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            //
+        });
+    }
+}
